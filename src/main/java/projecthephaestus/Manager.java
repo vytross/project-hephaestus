5457d7eb7b6e41ff41f0332cd866f8f5434cf6e1
vytross <71793587+vytross@users.noreply.github.com>
2021-05-05 19:45:30 -0400

    Create Manager.java

---
+++ src/main/java/projecthephaestus/Manager.java
@@ -0,0 +1,10 @@
+package projecthephaestus;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Manager implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
